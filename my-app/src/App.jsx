70905869e587b165be3474b10bc97802afa925c2
Lorenzo Zannetti <zannetti.lorenzo@gmail.com>
2024-07-21 18:02:24 +0200

    props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,14 @@
 import { HelloWorld } from "./HelloWorld";
 import { Message } from "./Message";
+import { Welcome } from "./Welcome";
 export function App() {
   return (
     <div>
       <HelloWorld />
       <hr />
       <Message />
+      <hr />
+      <Welcome name="Lorenzo" age={29} />
     </div>
   );
 }
---
+++ my-app/src/Welcome.jsx
@@ -0,0 +1,10 @@
+export function Welcome({ name, age }) {
+  return (
+    <div>
+      <h3>Welcome, {name}!</h3>
+      <strong>
+        <p>You are {age} year's old</p>
+      </strong>
+    </div>
+  );
+}
